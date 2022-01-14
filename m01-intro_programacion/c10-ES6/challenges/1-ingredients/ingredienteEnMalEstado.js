2db96656de4b50f1fe5e1502af139e6be7eff135
JavierArdiles <javierardilesreibel@gmail.com>
2022-01-14 20:34:50 -0300

    mentoria

--- m01-intro_programacion/c10-ES6/challenges/1-ingredients/ingredienteEnMalEstado.js
+++ m01-intro_programacion/c10-ES6/challenges/1-ingredients/ingredienteEnMalEstado.js
@@ -29,13 +29,39 @@ const menuDelDia = JSON.parse(fs.readFileSync('./ingredients.JSON', 'utf-8'));
 
 // console.log(ingredienteEnMalEstado(menuDelDia, "locro", "Tomates"));
 
-function ingredienteEnMalEstado(menu, comida, ingrediente){
-    if(!menu.hasOwnProperty(comida)) return "Lo siento, no tenemos eso en el menú";
-    let found = menu[comida].some(ingredientes => ingredientes == ingrediente);
-    if(!found) return "El menú está perfecto";
-    let i = menu[comida].indexOf(ingrediente);
-    let ingredientesEnMalEstado = [menu[comida][i - 1], menu[comida][i], menu[comida][i + 1]];
-    return ingredientesEnMalEstado;
-}
+// function ingredienteEnMalEstado(menu, comida, ingrediente){
+//     if(!menu.hasOwnProperty(comida)) return "Lo siento, no tenemos eso en el menú";
+//     let found = menu[comida].some(ingredientes => ingredientes == ingrediente);
+//     if(!found) return "El menú está perfecto";
+//     let i = menu[comida].indexOf(ingrediente);
+//     let ingredientesEnMalEstado = [menu[comida][i - 1], menu[comida][i], menu[comida][i + 1]];
+//     return ingredientesEnMalEstado;
+// }
 
-console.log(ingredienteEnMalEstado(menuDelDia, "raviolesConSalsa", "Pollo"));
+
+function ingredienteEnMalEstado(menu, comida, ingrediente) {
+    if (!menu[comida]) return 'No servimos esa comida';
+    let ingredienteEncontrado = menu[comida].find(elemento => elemento == ingrediente);
+    if (!ingredienteEncontrado) return "El menú está perfecto";
+    let indice = menu[comida].indexOf(ingredienteEncontrado);
+    let array = [];
+    if (menu[comida][indice - 1]) array.push(menu[comida][indice - 1]);
+    array.push(ingredienteEncontrado);
+    if (menu[comida][indice + 1]) array.push(menu[comida][indice + 1]);
+    return array;
+}
+console.log(ingredienteEnMalEstado(menuDelDia, "asdasdasd", "asdadasd"));
+
+function ingredienteEnMalEstado(menu, comida, ingrediente) {
+    if( !menu[comida] ) return "No servimos esa comida";
+    let array = [];
+    for(let i = 0; i < menu[comida].length; i++){
+        if( menu[comida][i] == ingrediente ) {
+            if( menu[comida][i - 1] ) array.push(menu[comida][i - 1]);
+            array.push(ingrediente);
+            if( menu[comida][i + 1] ) array.push(menu[comida][i + 1]);
+        }
+    };
+    if( array.length != 0 ) return array;
+    return "El menú está perfecto";
+}
